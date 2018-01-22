a89a4d82b7e46f61d8a4a924953eb2afb78b7d6c
Cameron Anderson and David Lewis <david@>
2018-01-22 11:35:51 -0800

    Date date component to form and display

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 //business logic
-function Task(name, description) {
+function Task(name, description, date) {
   this.name = name;
   this.description = description;
+  this.date = date;
 }
 
-function addItem(name, description) {
+function addItem(name, description, date) {
 
-  var outputName = "<h3>" + name + "</h3>";
+  var outputName = "<h3>" + name + " -Due: " + date.replace(/-/g, "/") + "</h3>";
   var outputDesc = "<p>" + description + "</p>";
   var outputDiv = "<div class='item'>" + outputName + outputDesc + "</div>"
   $("#list").append(outputDiv);
+}
+
+function hasClass(el, className) {
+  return el.classList.contains(className);
 }
 
 
@@ -20,17 +25,21 @@ $(document).ready(function() {
 
     var name = $("#new-name").val();
     var description = $("#new-description").val();
+    var dueDate = $("#dueDate").val();
 
-    var newTask = new Task(name, description);
+    var newTask = new Task(name, description. dueDate);
 
     $("#new-name").val("");
     $("#new-description").val("");
 
-    addItem(name, description);
+    addItem(name, description, dueDate);
 
     $(".item").last().click(function () {
       $(this).children("p").toggle();
-      $(this).addClass("complete");
+      if (hasClass(this, "complete"))
+        $(this).removeClass("complete");
+      else
+        $(this).addClass("complete");
     });
 
   });
